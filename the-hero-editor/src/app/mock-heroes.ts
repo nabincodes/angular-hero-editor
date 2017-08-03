aa86a0465528c7133b897ff104c4a86a000a7c97
nabincodes <nabinjaiswal@gmail.com>
2017-08-03 16:07:06 +0530

    added the service file

---
+++ the-hero-editor/src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] =  [
+  { id: 1, name:'Superman' },
+  { id: 2, name:'Batman' },
+  { id: 3, name:'Wonder Woman' },
+  { id: 4, name:'Flash' },
+  { id: 5, name:'Iron Man' },
+  { id: 6, name:'Spiderman' },
+  { id: 7, name:'Captain America'},
+  { id: 8, name:'Groot' },
+  { id: 9, name:'Rocket' },
+  { id: 10, name:'Joker' },
+  { id: 11, name:'Harley Quinn' },
+  { id: 12, name:'Deadshot' },
+  { id: 13, name:'BlackWindow' },
+  { id: 14, name:'Robin' },
+  { id: 15, name:'Green Lantern' },
+  { id: 16, name:'Ant Man' },
+];
